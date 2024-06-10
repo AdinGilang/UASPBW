3a273283339f253d946bfb26f114cfc8828e9dd7
Adin Gilang <ice919667@gmail.com>
2024-06-10 14:20:17 +0700

    kampusklik

---
+++ latihan5.php
@@ -0,0 +1,15 @@
+<?php
+
+// membuat variable
+
+$nama_mahasiswa = "Ariel Tatum";
+$nama_kamu = "Andi";
+
+echo "Hallo " . $nama_mahasiswa . " Selamat <br> datang, saya " . $nama_kamu;
+
+?>
+
+<html>
+    <title></title>
+    <body></body>
+</html>
